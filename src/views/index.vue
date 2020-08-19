9d523944e22e1e32b028c3d46da52d9fc30aef35
hooray <304327508@qq.com>
2020-08-19 10:46:10 +0800

    更新控制台页面

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,6 +2,26 @@
     <div>
         <page-main>
             <h1>欢迎使用 vue-automation-admin</h1>
+            <div class="links">
+                <el-link href="http://eoner.gitee.io/vue-automation-admin" target="_blank">开发文档</el-link>
+                <span class="divided" />
+                <el-link href="https://gitee.com/eoner/vue-automation-admin" target="_blank">项目地址</el-link>
+            </div>
         </page-main>
     </div>
 </template>
+
+<style lang="scss" scoped>
+h1 {
+    margin: 0 0 20px;
+}
+.links {
+    display: flex;
+    align-items: center;
+    .divided {
+        height: 12px;
+        margin: 0 10px;
+        border: 1px solid #c0c4cc;
+    }
+}
+</style>
