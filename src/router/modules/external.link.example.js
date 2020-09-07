6764a0428d4d8ccfcb29616debfefa87e0601260
hooray <304327508@qq.com>
2020-09-07 18:32:33 +0800

    增加iframe模式

--- src/router/modules/external.link.example.js
+++ src/router/modules/external.link.example.js
@@ -1,11 +1,29 @@
 import Layout from '@/layout'
+import IframeLayout from '@/layout/iframe'
 
 export default {
-    path: 'http://eoner.gitee.io/vue-automation-admin',
+    path: '/link',
     component: Layout,
     name: 'externalLinkExample',
     meta: {
         title: '官网',
         icon: 'sidebar-external-link'
-    }
+    },
+    children: [
+        {
+            path: 'iframe',
+            component: IframeLayout,
+            name: 'linkExampleIframe',
+            meta: {
+                title: '内嵌网页',
+                link: 'http://eoner.gitee.io/vue-automation-admin'
+            }
+        },
+        {
+            path: 'http://eoner.gitee.io/vue-automation-admin',
+            meta: {
+                title: '新窗口打开'
+            }
+        }
+    ]
 }
