06dae7c1b3fb4bb8bd96def7493eeef3beb985f5
hooray <304327508@qq.com>
2020-09-07 17:22:11 +0800

    优化404页面

--- src/views/404.vue
+++ src/views/404.vue
@@ -4,13 +4,27 @@
         <div class="content">
             <h1>404</h1>
             <div class="desc">抱歉，你访问的页面不存在</div>
-            <el-button type="primary" @click="goBack">返回首页</el-button>
+            <el-button type="primary" @click="goBack">{{ countdown }}秒后，返回首页</el-button>
         </div>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            countdown: 5
+        }
+    },
+    mounted() {
+        const inter = setInterval(() => {
+            this.countdown--
+            if (this.countdown == 0) {
+                clearInterval(inter)
+                this.goBack()
+            }
+        }, 1000)
+    },
     methods: {
         goBack() {
             this.$router.push('/')
