f5f41a1f3999754dbf9c3fa2c76bbf7714f03cf9
hooray <304327508@qq.com>
2020-09-21 15:25:20 +0800

    更新演示

--- docs/demo/js/header_example.2cbaa3b5.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["header_example"],{e2f8:function(e,n,t){"use strict";t.r(n);var s=t("2877"),i=Object(s.a)({},(function(){var e=this.$createElement,n=this._self._c||e;return n("div",[n("page-main",[this._v(" 请在 setting.js 里开启头部导航，并在 router/index.js 修改配置 ")])],1)}),[],!1,null,null,null);n.default=i.exports}}]);
